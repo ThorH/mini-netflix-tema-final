c0e7eb09aff96e97a110cf9100b0d15c89db0a6f
ilegra <haubertthor@gmail.com>
2022-09-13 17:21:56 -0300

    Profile page component

---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,27 @@
+import {
+    ProfileContainer
+} from "./ProfileStyle"
+import { BackToHomeContainer } from "../../AppStyles";
+import { BiArrowBack } from "react-icons/bi"
+import { Link } from "react-router-dom";
+import { useUser } from "../../hooks/useUser"
+
+function Profile() {
+    const { userLogged } = useUser()
+
+    return (
+        <ProfileContainer>
+            <BackToHomeContainer>
+                <Link to="/">
+                    <BiArrowBack size={30} color="#ffffff" />
+                </Link>
+            </BackToHomeContainer>
+            <h2>{userLogged?.name}</h2>
+            <img src={userLogged?.picture} alt={`${userLogged?.name} picture`} />
+            <p>Email: {userLogged?.email}</p>
+            <p>Country: {userLogged?.country}</p>
+        </ProfileContainer>
+    )
+}
+
+export default Profile;
---
+++ src/pages/Profile/ProfileStyle.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const ProfileContainer = styled.div`
+    background-color: #141414;
+    text-align: center;
+    width: 500px;
+    height: 450px;
+    padding: 50px;
+    border-radius: 4px;
+
+    h2 {
+        color: #ffffff;
+        margin-bottom: 10px;
+    }
+
+    img {
+        width: 300px;
+        height: 300px;
+        object-fit: cover;
+        border-radius: 50%;
+    }
+
+    p {
+        text-align: start;
+        color: #ffffff;
+        font-size: 16px;
+        font-weight: 600;
+        margin: 15px 50px;
+    }
+`;
+
